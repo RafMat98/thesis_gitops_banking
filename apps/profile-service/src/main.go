e64fe321170af60574a452054273ea48c57a3a5e
RafMat Alias <rafaelmat2598@gmail.com>
2026-05-18 22:42:35 +0300

    testing CI_Phase

--- apps/profile-service/src/main.go
+++ apps/profile-service/src/main.go
@@ -40,6 +40,7 @@ func main() {
 		Password: redisPass,
 		DB:       0,
 	})
+
 	// 2. Kafka Consumer (mTLS)
 	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers":                   kafkaBrokers,
